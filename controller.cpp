4f5fbfaf2fef9d1b6a5a9b9c9ee372de9ae38ddb
Jørgen Bang Sneisen <jorgen.sneisen@lf-nett.no>
2026-05-23 22:39:57 +0200

    Refactor cpp controller

--- controller.cpp
+++ controller.cpp
@@ -28,26 +28,30 @@ double dt = 0.01;
 double Kx = 0.1;
 double Kv = 1.0;
 
+
+Output PD_Controller(Input input) {
+	Output output {force: 0};
+
+	double error = setpoint - input.angle;
+	
+	double proportional = -Kp * error;
+	double derivative = -Kd * (error - previous_error) / dt;
+	double cart_position = -Kx * input.cart_position;
+	double cart_velocity = - Kv * input.cart_velocity;
+
+	output.force = proportional + derivative - cart_position - cart_velocity;
+	
+	previous_error = error;
+	return output;
+}
+
 int main() {
 	std::string input;
 	while (std::getline(std::cin, input)) {
 
 	Input state = json::parse(input).get<Input>();
 	
-	Output output {force: 0};
-
-	double error = setpoint - state.angle;
-	
-	double proportional = -Kp * error;
-	double derivative = -Kd * (error - previous_error) / dt;
-	double cart_position = -Kx * state.cart_position;
-	double cart_velocity = - Kv * state.cart_velocity;
-
-	output.force = proportional + derivative - cart_position - cart_velocity;
-	
-	json json_output = output;	
-	
-	previous_error = error;
+	json json_output = PD_Controller(state);	
 
 	std::cout << json_output.dump() << std::endl;
 	}
